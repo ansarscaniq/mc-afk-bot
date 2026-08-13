bedd9ed7c9c2696dfd16a4899b3a20e230f4003b
ansarscaniq <ansar.temirkhan09.04.2013@gmail.com>
2026-08-13 22:38:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const mineflayer = require('mineflayer');
 const http = require('http');
 
-// 1. Создаём мини-веб-сервер для получения бесплатного Web Service на Render
 http.createServer((req, res) => {
     res.write("AFK Bot is alive and running 24/7!");
     res.end();
 }).listen(process.env.PORT || 3000);
 
-// 2. Функция запуска и поддержки бота
 function createBot() {
     const bot = mineflayer.createBot({
-        host: 'critreason.aternos.me',
-        port: 33086,                   // Твой текущий порт Aternos
-        username: 'AFK_Bot_Render',    // Ник бота в игре
-        version: false                 // Авто-определение версии сервера
+        host: 'CritReason.aternos.me',
+        port: 33086,
+        username: 'AFK_Bot_Render',
+        checkTimeoutInterval: 60 * 1000 // Увеличиваем таймаут для стабильного входа
     });
 
     bot.on('spawn', () => {
         console.log('✅ Бот успешно зашел на сервер!');
     });
 
-    // 3. Анти-AFK: прыжок каждые 40 секунд против кика за инактивити
+    // Анти-AFK прыжок каждые 40 секунд
     bot.on('spawn', () => {
         setInterval(() => {
             if (bot && bot.entity) {
@@ -30,7 +28,6 @@ function createBot() {
         }, 40000);
     });
 
-    // 4. Авто-переподключение при вылете
     bot.on('end', () => {
         console.log('⚠️ Отключение от сервера. Переподключение через 15 секунд...');
         setTimeout(createBot, 15000);
